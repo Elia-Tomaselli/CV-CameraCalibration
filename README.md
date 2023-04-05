5e7432ec52cfffceac6b129a8d8147ed85aa9ea1
EliaTomaselli <Elia.tomaselli@gmail.com>
2023-04-05 12:47:41 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Progetto di Computer Vision
+
+[Link](https://drive.google.com/drive/folders/1tIRdiU4CT_9tqHtj1EaxWvbHOXSfvXn5?usp=sharing) della cartella Google Drive con i video.
+
+[Link](https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html) della videocamera installata al sanba.
+
+[Link](https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html) dato dal tutor delle risorse OpenCV per la calibrazione intrinseca.
---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+from os import path
+
+VIDEO_DIR = "videos"
+
+video_paths = [
+    path.join(VIDEO_DIR, "out14.mp4"),
+    path.join(VIDEO_DIR, "out20.mp4"),
+    path.join(VIDEO_DIR, "out42.mp4")
+]
+
+video = cv.VideoCapture(video_paths[0])
+
+# Verifica file video valido
+if not video.isOpened():
+    print("Errore nel caricamento del file")
+    exit()
+
+while True:
+    # Lettura frame dal video
+    ret, frame = video.read()
+
+    # Verifica fine video
+    if not ret:
+        break
+
+    cv.imshow("Frame", frame)
+    
+    if cv.waitKey(25) == ord('q'):
+        break
+
+video.release()
+cv.destroyAllWindows()
