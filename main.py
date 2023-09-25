572dde24f0272b3d7e81d8f5f09d993659772af5
elrich2610 <alessandrorizzetto26@gmail.com>
2023-09-25 19:03:14 +0200

    video capture calibration

--- main.py
+++ main.py
@@ -1,33 +1,87 @@
+import numpy as np
 import cv2 as cv
-from os import path
 
-VIDEO_DIR = "videos"
+# Define chessboard dimensions
+CHESSBOARD_WIDTH = 5
+CHESSBOARD_HEIGHT = 7
 
-video_paths = [
-    path.join(VIDEO_DIR, "out14.mp4"),
-    path.join(VIDEO_DIR, "out20.mp4"),
-    path.join(VIDEO_DIR, "out42.mp4")
-]
+# Termination criteria for corner detection
+termination_criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-video = cv.VideoCapture(video_paths[0])
+# Prepare object points, like (0, 0, 0), (1, 0, 0), (2, 0, 0), ..., (6, 5, 0)
+object_points = np.zeros((CHESSBOARD_WIDTH * CHESSBOARD_HEIGHT, 3), np.float32)
+object_points[:, :2] = np.mgrid[0:CHESSBOARD_WIDTH, 0:CHESSBOARD_HEIGHT].T.reshape(-1, 2)
 
-# Verifica file video valido
-if not video.isOpened():
-    print("Errore nel caricamento del file")
-    exit()
+# Arrays to store object points and image points from the video frames
+world_points = []  # 3D point in real-world space
+image_points = []  # 2D points in image plane
 
-while True:
-    # Lettura frame dal video
-    ret, frame = video.read()
+# Open the video file
+video_capture = cv.VideoCapture('video/out1F.mp4')  # Replace 'your_video.mp4' with the path to your video file
 
-    # Verifica fine video
+frame_count = int(video_capture.get(cv.CAP_PROP_FRAME_COUNT))
+print(f"Total frames in the video: {frame_count}")
+
+desired_start_frame = 500
+video_capture.set(cv.CAP_PROP_POS_FRAMES, desired_start_frame) # Set the starting frame of the video capture
+
+frame_count = 0
+max_saved_frames = 30
+output_folder = 'saved_frames'
+
+while frame_count < max_saved_frames:
+    # Read a frame from the video
+    ret, frame = video_capture.read()
     if not ret:
-        break
+        break  # Break the loop if we've reached the end of the video
 
-    cv.imshow("Frame", frame)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    found_corners, corners = cv.findChessboardCorners(gray, (CHESSBOARD_WIDTH, CHESSBOARD_HEIGHT), None)
+    print(found_corners)
     
-    if cv.waitKey(25) == ord('q'):
-        break
+    if found_corners:
+        frame_count += 1
 
-video.release()
+        # Refine corner positions
+        refined_corners = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), termination_criteria)
+        
+        # Save the frame with detected corners
+        frame_filename = f"{output_folder}/frame_{frame_count}.jpg"
+        cv.imwrite(frame_filename, frame)
+
+        # Add object points and image points
+        world_points.append(object_points)
+        image_points.append(refined_corners)
+
+        # Draw and display the corners (optional)
+        cv.drawChessboardCorners(frame, (CHESSBOARD_WIDTH, CHESSBOARD_HEIGHT), refined_corners, found_corners)
+        cv.imshow('Frame', frame)
+        cv.waitKey(100)  # Adjust the delay as needed
+
+video_capture.release()
 cv.destroyAllWindows()
+
+# Calibrate the camera using the saved frames
+if len(image_points) >= max_saved_frames:
+    calibration_success, camera_matrix, distortion_coeffs, _, _ = cv.calibrateCamera(
+        world_points, image_points, gray.shape[::-1], None, None)
+
+    # Print the calibration results
+    if calibration_success:
+        print("Camera matrix:")
+        print(camera_matrix)
+        print("\nDistortion coefficients:")
+        print(distortion_coeffs)
+    else:
+        print("Calibration failed.")
+else:
+    print(f"Not enough frames with chessboard found. Found {len(image_points)} frames, required {max_saved_frames}.")
+
+# test undistortion on an image
+img = cv.imread('samples/test_undist.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(camera_matrix, distortion_coeffs, (w,h), 1, (w,h))
+new_image = cv.undistort(img, camera_matrix, distortion_coeffs, None, newcameramtx)
+cv.imwrite('samples/undistorted.jpg', new_image)
