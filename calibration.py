4b0827fb641dc9e7c0b1e2a83c473de9be62773c
Elia-Tomaselli <elia.tomaselli@studenti.unitn.it>
2023-09-28 16:47:49 +0200

    Seconda prova calibrazione con linee del campo

--- calibration.py
+++ calibration.py
@@ -17,30 +17,92 @@ IMAGES_NAMES = [
     'out8.png',
 ]
 
-# Prepare object points, like (0, 0, 0), (1, 0, 0), (2, 0, 0), ..., (6, 5, 0)
-worldPoints = np.array([[
-    [0, 0, 0],
-    [6, 0, 0],
-    [12, 0, 0],
-    [18, 0, 0],
+# The image used for the calibration, change it if you change the points used for calibration
+IMAGE_NAME = IMAGES_NAMES[5]
 
-    [0, 9, 0],
-    [6, 9, 0],
-    [12, 9, 0],
-    [18, 9, 0],
+IMAGE_PATH = path.join(IMAGES_PATH, IMAGE_NAME)
 
-    # [0, -1.75, 0],
-    # [6, -1.75, 0],
-    # [12, -1.75, 0],
-    # [18, -1.75, 0],
+img = cv.imread(IMAGE_PATH)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    # [0, 9 + 1.75, 0],
-    # [6, 9 + 1.75, 0],
-    # [12, 9 + 1.75, 0],
-    # [18, 9 + 1.75, 0]
-]], np.float32)
+# World points and image points for image "out2.png"
+# volleyballWorldPoints = [
+#     [-9.0, -4.5, 0.0],
+#     [-3.0, -4.5, 0.0],
+#     [3.0, -4.5, 0.0],
+#     [9.0, -4.5, 0.0],
 
-imagePoints = np.array([[
+#     [-9.0, 4.5, 0.0],
+#     [-3.0, 4.5, 0.0],
+#     [3.0, 4.5, 0.0],
+#     [9.0, 4.5, 0.0],
+
+#     [-9.0, -6.25, 0.0],
+#     [-3.0, -6.25, 0.0],
+#     [3.0, -6.25, 0.0],
+#     [9.0, -6.25, 0.0],
+
+#     [-9.0, 6.25, 0.0],
+#     [-3.0, 6.25, 0.0],
+#     [3.0, 6.25, 0.0],
+#     [9.0, 6.25, 0.0],
+
+#     [-6.0, -4.5, 0.0],
+#     [0.0, -4.5, 0.0],
+#     [6.0, -4.5, 0.0],
+# ]
+# volleyballImagePoints = [
+#     [745, 1347],
+#     [1431, 1380],
+#     [2242, 1392],
+#     [2945, 1380],
+
+#     [1096, 1042],
+#     [1584, 1044],
+#     [2108, 1052],
+#     [2602, 1065],
+
+#     [645, 1442],
+#     [1382, 1493],
+#     [2286, 1506],
+#     [3039, 1477],
+
+#     [1144, 1003],
+#     [1603, 1005],
+#     [2091, 1012],
+#     [2551, 1025],
+
+#     [1088, 1367],
+#     [1833, 1390],
+#     [2594, 1390],
+# ]
+
+# basketballWorldPoints = [
+#     [-14.0, -7.5, 0.0],
+#     [14.0, -7.5, 0.0],
+#     [-14.0, 7.5, 0.0],
+#     [14.0, 7.5, 0.0],
+# ]
+# basketballImagePoints = [
+#     [150, 1458],
+#     [3551, 1507],
+#     [859, 982],
+#     [2848, 1013],
+# ]
+
+# World points and image points for image "out6.png"
+volleyballWorldPoints = [
+    [-9.0, -4.5, 0],
+    [-3.0, -4.5, 0],
+    [3.0, -4.5, 0],
+    [9.0, -4.5, 0],
+
+    [-9.0, 4.5, 0],
+    [-3.0, 4.5, 0],
+    [3.0, 4.5, 0],
+    [9.0, 4.5, 0],
+]
+volleyballImagePoints = [
     [268, 1632],
     [1198, 1775],
     [2579, 1790],
@@ -50,25 +112,14 @@ imagePoints = np.array([[
     [1519, 928],
     [2278, 936],
     [2938, 964],
+]
 
-    # [126, 1848],
-    # [1091, 2097],
-    # [2676, 2116],
-    # [3650, 1888],
+basketballWorldPoints = []
+basketballImagePoints = []
 
-    # [942, 866],
-    # [1555, 847],
-    # [2244, 855],
-    # [2854, 885]
-]], np.float32)  # 2D points in image plane
-
-# Arrays to store object points and image points from all the images
-
-# Get the paths of all images in the samples folder as a list
-imagePath = path.join(IMAGES_PATH, IMAGES_NAMES[5])
-
-img = cv.imread(imagePath)
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+worldPoints = np.array(
+    [volleyballWorldPoints + basketballWorldPoints], dtype=np.float32)
+imagePoints = np.array([volleyballImagePoints + basketballImagePoints], dtype=np.float32)
 
 # Calibrate the camera
 calibrationSuccess, cameraMatrix, distortionCoeffs, rotationVecs, translationVecs = cv.calibrateCamera(
@@ -85,17 +136,17 @@ pprint(rotationVecs)
 print("\nTranslation vectors:")
 print(translationVecs)
 
-width, height = gray.shape[:2]
+width, height=gray.shape[:2]
 
 # Get the optimal new camera matrix and region of interest
-optimalCameraMatrix, roi = cv.getOptimalNewCameraMatrix(
+optimalCameraMatrix, roi=cv.getOptimalNewCameraMatrix(
     cameraMatrix, distortionCoeffs, (width, height), 1, (width, height))
 
 print("\nOptimal camera matrix: ")
 print(optimalCameraMatrix)
 
 # Undistort
-undistortedImg = cv.undistort(
+undistortedImg=cv.undistort(
     img, cameraMatrix, distortionCoeffs, None, optimalCameraMatrix)
 
 # crop the image
@@ -105,12 +156,12 @@ undistortedImg = cv.undistort(
 cv.imwrite('result.png', undistortedImg)
 
 # Reprojection error
-meanError = 0
+meanError=0
 for i in range(len(worldPoints)):
-    imagePoints2, _ = cv.projectPoints(
+    imagePoints2, _=cv.projectPoints(
         worldPoints[i], rotationVecs[i], translationVecs[i], cameraMatrix, distortionCoeffs)
-    imagePoints2 = imagePoints2.reshape(imagePoints.shape)
-    error = cv.norm(imagePoints[i], imagePoints2[i],
+    imagePoints2=imagePoints2.reshape(imagePoints.shape)
+    error=cv.norm(imagePoints[i], imagePoints2[i],
                     cv.NORM_L2) / len(imagePoints2)
     meanError += error
-print("total error: {}".format(meanError/len(worldPoints)))
+print("total error: {}".format(meanError / len(worldPoints)))
